42b42767c9d66382e793d12fe5f8820a902d33f1
Alberto Barradas <abcsds@gmail.com>
2019-07-07 17:33:51 +0200

    Update model: Add n_channels parameter Add conv layer Remove resizing

--- Model.py
+++ Model.py
@@ -11,15 +11,19 @@ from time import time
 # is the optimizer included in the model class? (sklearn style)
 # logging for tensorboard (summaries)
 class BetaVAE:
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
     def __init__(self,
+                 path="./ck",
                  img_side=128,
-                 batch_size=10,
+                 batch_size=16,
                  epochs=10,
                  n_z=10,
-                 beta=1.3,
+                 beta=100,
                  lr=1e-3,
-                 epsilon=1e-8):
-        self.AUTOTUNE   = tf.data.experimental.AUTOTUNE
+                 format="jpg",
+                 extra_repeats=2,
+                 n_channels=1,
+                 epsilon=1e-10):
         self.img_side   = img_side
         self.img_size   = self.img_side**2
         self.batch_size = batch_size
@@ -28,9 +32,16 @@ class BetaVAE:
         self.beta       = beta
         self.lr         = lr
         self.epsilon    = epsilon
+        self.n_channels = n_channels
 
+        # Build Model Architecture
         tf.reset_default_graph()
 
+        # Data loading
+        self.load_data(path, format=format, extra_repeats=extra_repeats)
+        self.next_element = self.create_iterator()
+
+        # Placeholders
         self.input = tf.placeholder(tf.float32,
                                     [None, self.img_size],
                                     name="Inputs")
@@ -38,11 +49,13 @@ class BetaVAE:
                                      [None, self.n_z],
                                      name="Latents")
 
-        self.encoder(self.input)
-        self.decoder(self.z)
-        # self.output = self.model
-        self.gen_img = self.decoder(self.latent)
+        # Encoder, decoder and sampling
+        self.encode(self.input) # z, mu, and lss are saved in the object.
+        self.model = self.decode(self.z)
 
+        self.sample = self.decode(self.latent)
+
+        # LOSSES
         # Reconstruction loss
         # Minimize the cross-entropy loss
         # H(x, x_hat) = -\Sigma x*log(x_hat) + (1-x)*log(1-x_hat)
@@ -65,22 +78,12 @@ class BetaVAE:
         # Total loss
         self.loss = self.recon_loss + self.beta * self.latent_loss
         self.plottableLoss = []
+
         # Optimizer
-        self.opt = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss)
+        opt = tf.train.AdamOptimizer(learning_rate=self.lr)
+        self.opt = opt.minimize(self.loss)
 
-    def fit(self, sess, path):
-        self.load_data(path)
-        # Create Tensorflow Iterator
-        path_ds = tf.data.Dataset.from_tensor_slices(self.all_image_paths)
-        ds = path_ds.map(self.load_and_preprocess_image,
-                         num_parallel_calls=self.AUTOTUNE)
-        ds = ds.shuffle(buffer_size=self.batch_size * 2)
-        ds = ds.repeat(self.data_repeats)
-        ds = ds.prefetch(buffer_size=self.AUTOTUNE)
-        ds = ds.batch(self.batch_size)
-        self.iterator = ds.make_initializable_iterator()
-        self.next_element = self.iterator.get_next()
-
+    def fit(self, sess):
         print("Begin Session: Processing {:d} images in {:d} batches".format(
               self.batch_size * self.n_batches * self.epochs, self.n_batches))
         # Session
@@ -94,8 +97,8 @@ class BetaVAE:
                 for i in range(self.n_batches - 2):
                     print(".", end="")
                     X = self.get_next(sess)
-                    sess.run(self.opt, feed_dict={input: X})
-                    tloss = sess.run(self.loss, feed_dict={input: X})
+                    tloss, _ = sess.run([self.loss, self.opt],
+                                        feed_dict={self.input: X})
                     if np.isnan(tloss) or np.isinf(tloss):
                         print("Error: loss went cray cray")
                         self.err = True
@@ -109,54 +112,64 @@ class BetaVAE:
                 pass
 
     def predict(self, sess, x):
-        return sess.run(self.model, feed_dict={input:x})
+        return sess.run(self.model, feed_dict={self.input: x})
+
+    def create_iterator(self):
+        # Create Tensorflow Iterator
+        path_ds = tf.data.Dataset.from_tensor_slices(self.all_image_paths)
+        ds = path_ds.map(self.load_and_preprocess_image,
+                         num_parallel_calls=self.AUTOTUNE)
+        ds = ds.shuffle(buffer_size=self.batch_size * 2)
+        ds = ds.repeat(self.data_repeats)
+        ds = ds.prefetch(buffer_size=self.AUTOTUNE)
+        ds = ds.batch(self.batch_size)
+        self.iterator = ds.make_initializable_iterator()
+        return self.iterator.get_next()
 
     def get_next(self, sess):
-        return sess.run(self.next_element)
+        return sess.run(tf.layers.Flatten()(self.next_element))
 
-    def encoder(self, x):
+    def encode(self, x):
         with tf.variable_scope("Encoder", reuse=tf.AUTO_REUSE):
-            # dim = x.get_shape().as_list() # https://datascience.stackexchange.com/questions/30810/gan-with-conv2d-using-tensorflow-shape-error
-            # x = tf.reshape(x, shape = [-1, *dim[1:]])
-            self.model = tf.reshape(x, [-1, self.img_side, self.img_side, 1])
-            self.model = tf.layers.Conv2D(filters=6 , kernel_size=3, strides=(2, 2), activation=tf.nn.relu)(self.model)
-            self.model = tf.layers.Conv2D(filters=12, kernel_size=3, strides=(2, 2), activation=tf.nn.relu)(self.model)
-            self.model = tf.layers.Conv2D(filters=24, kernel_size=3, strides=(2, 2), activation=tf.nn.relu)(self.model)
-            self.model = tf.layers.Flatten()(self.model)
-            self.mu = tf.layers.Dense(self.n_z)(self.model)
-            self.log_sigma_sq = tf.layers.Dense(self.n_z)(self.model)
+            model = tf.reshape(x, [-1, self.img_side, self.img_side, self.n_channels])
+            model = tf.layers.Conv2D(filters=6 , kernel_size=3, strides=(2, 2), activation=tf.nn.relu)(model)
+            model = tf.layers.Conv2D(filters=12, kernel_size=3, strides=(2, 2), activation=tf.nn.relu)(model)
+            model = tf.layers.Conv2D(filters=18, kernel_size=3, strides=(2, 2), activation=tf.nn.relu)(model)
+
+            model = tf.layers.Flatten()(model)
+            self.mu = tf.layers.Dense(self.n_z)(model)
+            self.log_sigma_sq = tf.layers.Dense(self.n_z)(model)
             self.eps = tf.random_normal(shape=tf.shape(self.log_sigma_sq),
                                         mean=0, stddev=1, dtype=tf.float32)
             self.z = self.mu + tf.sqrt(tf.exp(self.log_sigma_sq)) * self.eps
             # return z, log_sigma_sq, mu
 
-    def decoder(self, x):
-        with tf.variable_scope("Decoder", reuse=tf.AUTO_REUSE):
-            self.model = tf.layers.Dense(7 * 7 * 32)(x)
-            self.model = tf.reshape(self.model, [-1, 7, 7, 32])
-            self.model = tf.layers.Conv2DTranspose(filters=24, kernel_size=3, strides=(2, 2), padding="same", activation=tf.nn.relu)(self.model)
-            self.model = tf.layers.Conv2DTranspose(filters=12, kernel_size=3, strides=(2, 2), padding="same", activation=tf.nn.relu)(self.model)
-            self.model = tf.layers.Conv2DTranspose(filters=6 , kernel_size=3, strides=(2, 2), padding="same", activation=tf.nn.relu)(self.model)
-            self.model = tf.layers.Flatten()(self.model)
-            self.model = tf.layers.Dense(self.img_size,
-                                         activation=tf.nn.sigmoid)(self.model)
-            # return self.model
+    def decode(self, x):
+        with tf.variable_scope("decoder", reuse=tf.AUTO_REUSE):
+            model = tf.layers.Dense(7*7*32)(x)
+            model = tf.reshape(model, [-1, 7, 7, 32])
+            model = tf.layers.Conv2DTranspose(filters=18, kernel_size=3, strides=(2, 2), padding="same", activation=tf.nn.relu)(model)
+            model = tf.layers.Conv2DTranspose(filters=12, kernel_size=3, strides=(2, 2), padding="same", activation=tf.nn.relu)(model)
+            model = tf.layers.Conv2DTranspose(filters=6, kernel_size=3, strides=(2, 2), padding="same", activation=tf.nn.relu)(model)
+            model = tf.layers.Flatten()(model)
+            model = tf.layers.Dense(self.img_size, activation=tf.nn.sigmoid)(model)
+        return model
 
-    def load_data(self, path, format="jpg"):
-        self.data_root = pathlib.Path(path)
-        self.all_image_paths = list(self.data_root.glob("*." + format))
-        self.all_image_paths = [str(path) for path in self.all_image_paths]
+    def load_data(self, path, format="jpg", extra_repeats=2):
+        data_root = pathlib.Path(path)
+        all_image_paths = list(data_root.glob("*." + format))
+        self.all_image_paths = [str(path) for path in all_image_paths]
         random.shuffle(self.all_image_paths)
         self.image_count = len(self.all_image_paths)
-        self.data_repeats = self.epochs * 2
+        self.data_repeats = self.epochs * extra_repeats
         self.n_batches = self.image_count // self.batch_size
 
     def preprocess_image(self, image):
-        image = tf.image.decode_jpeg(image, channels=1)
+        image = tf.image.decode_jpeg(image, channels=self.n_channels)
         # image = tf.image.resize_images(image, [self.img_side, self.img_side])
         # image = tf.image.per_image_standardization(image)
         # image = image + tf.reduce_min(image)
-        # image /= tf.reduce_max(image)
+        image /= tf.reduce_max(image)
         return image
 
     def load_and_preprocess_image(self, path):
